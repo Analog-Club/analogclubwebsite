3e6007440086103d5dc89fe5cf127c3e42752aec
Caleb_Lykken <Lykken@uw.edu>
2024-05-20 17:34:39 -0700

    added link to the title

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
   return (
     <div className="Navbar">
       <div className="title-container">
-        <h1 className="title-text">ANALOG CLUB</h1>
+      <NavLink to ="/home" style={{ textDecoration: 'none' }} ><h1 className="title-text">ANALOG CLUB</h1></NavLink>
       </div>
       <div className="navbar-links">
         <div className="nav-container">
